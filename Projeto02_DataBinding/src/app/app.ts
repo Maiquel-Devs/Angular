cdbca90344a83bad555fdf3f0d59f6ddb6ef5364
Maiquel <junior.mafra13062005@gmail.com>
2026-04-20 21:53:02 -0300

    Apredendo Angularf

---
+++ Projeto02_DataBinding/src/app/app.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  // Criando um sinal numérico inicializado em 0
+  protected contador = signal(0);
+
+  // Método para incrementar o valor
+  incrementar() {
+    this.contador.update(valor => valor + 1);
+  }
+
+  // Método para decrementar o valor
+  decrementar() {
+    this.contador.update(valor => valor - 1);
+  }
+}
