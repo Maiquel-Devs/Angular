b21b774c3044f339dd21c687991c9e38062ac9f0
Maiquel <junior.mafra13062005@gmail.com>
2026-04-30 17:19:21 -0300

    Estudando Simulação Crud  Angular

---
+++ Projeto05_CRUDLocal/src/app/app.ts
@@ -0,0 +1,36 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+})
+export class App {
+  
+  // Simulação do Banco de Dados em Memória (Lista de produtos)
+  listaProdutos = signal(['Notebook', 'Mouse', 'Teclado'])
+
+  adicionar(novo : string) {
+    if(novo) {
+      // Adiciona o novo item no final da lista
+      this.listaProdutos.update(atual => [...atual, novo]);
+    }
+  }
+
+  remover(itemNome : string) {
+    // Remove o item da lista
+    this.listaProdutos.update(atual => atual.filter(p => p !== itemNome));
+  }
+
+}
+
+/*
+
+- Aqui o Arrays são imutáveis, por isso usamos o update para criar uma nova lista
+- No @Input não podemos usar o 
+
+-- As vantagens dos Arrais imutáveis é que eles são mais rápidos e leves que os Arrais mutáveis, 
+pois eles não precisam recarregar a página toda vez que um item é adicionado ou removido
+
+
+*/ 
